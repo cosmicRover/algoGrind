ea12a2da802a5ec66ac484296d5aa9466da64ba9
Joy Paul <jpaul3250@gmail.com>
2019-07-19 20:30:46 -0400

    powerset

---
+++ Powerset.py
@@ -0,0 +1,18 @@
+# Problem M
+# Solution
+
+# Time O(n*2^n) | Space O(n*2^n)
+
+def powerset(inputArray):
+    subsets = [[]]
+
+    # iterate through the inputArray and the subsets
+    # append current element along with the current subset
+    for element in inputArray:
+        for i in range(len(subsets)):
+            currentSubset = subsets[i]
+            subsets.append(currentSubset + [element])
+    return subsets
+
+input = [1, 2, 3]
+print(powerset(input))
