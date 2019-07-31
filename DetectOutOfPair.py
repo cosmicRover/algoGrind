2757901056ef3f3c7970f03008bdf905cf6b14e8
Joy Paul <jpaul3250@gmail.com>
2019-07-31 00:07:17 -0400

    outofloop

---
+++ DetectOutOfPair.py
@@ -0,0 +1,16 @@
+#Given an array nums of length n. All elements appear in pairs except one of them. Find this single element that appears alone.
+#Pairs of the same element cannot be adjacent:
+
+def findOutOfPair(pairArray):
+
+    pointer = 1
+
+    for _ in range(0, len(pairArray) - 2):
+        if pairArray[pointer - 1] != pairArray[pointer]:
+            print("Found the misMatch %s" %pairArray[pointer -1])
+            return
+        pointer += 2 
+
+
+inputArray = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2]
+findOutOfPair(inputArray)
