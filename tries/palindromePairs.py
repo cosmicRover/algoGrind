e6849ba56d713474d32e72cffc96158bb58108fc
joypaul <jpaul3250@gmail.com>
2019-10-08 20:47:44 -0400

    no luck with trie

--- tries/palindromePairs.py
+++ tries/palindromePairs.py
@@ -41,17 +41,14 @@ class Solution:
 
         # palindroome matching needs more work
         for level in range(length):
-            if pCrawler is None:
-                break
-
             index = self.cnvrtChrToUni(word[level])
             ref = pCrawler.inputRef
-
             #gets the values that are palindrom
             if pCrawler.children[index]:
                 palString += word[level]
                 print(palString) 
-
+            
+            #if a char dont exist, what should happen to the rest of the chars ona word?
             if not pCrawler.children[index]:
                 return -1
              
@@ -60,22 +57,31 @@ class Solution:
         return ref
 
     def palindromePairs(self, words: [str]) -> [[int]]:
-
         pairs = []
-        
         for word in words:
             self.insert(word, words.index(word))
 
         for word in words:
             returnVal = self.search(word)
-            
             if returnVal != -1:
                 pairs.append([words.index(word), returnVal])
 
         print(pairs)
             
-
-
 s = Solution()
 inputs = ["abcd","dcba","lls","s","sssll"]
 s.palindromePairs(inputs)
+
+# here is one that doesn't use tries
+# uses a dict and usually easier to understand 
+
+# def palindromePairs(self, words):
+#     d, res = dict([(w[::-1], i) for i, w in enumerate(words)]), []
+#     for i, w in enumerate(words):
+#         for j in range(len(w)+1):
+#             prefix, postfix = w[:j], w[j:]
+#             if prefix in d and i != d[prefix] and postfix == postfix[::-1]:
+#                 res.append([i, d[prefix]])
+#             if j > 0 and postfix in d and i != d[postfix] and prefix == prefix[::-1]:
+#                 res.append([d[postfix], i])
+#     return res
