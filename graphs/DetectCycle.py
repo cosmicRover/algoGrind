a2fb449556f8704b88e2e44c4cced44fdf22d2ca
Joy Paul <jpaul3250@gmail.com>
2019-12-13 21:02:15 -0500

    cycle detection

---
+++ graphs/DetectCycle.py
@@ -0,0 +1,36 @@
+class DetecyCycle:
+    def detect_cycle(self, nums:[[int]]):
+
+        #init ds to hold graph and visited values
+        #can reduce the amount of unnecessary space by having # of nodes inputted directly
+        #or validating 
+        graph = [set() for _ in range(len(nums))]
+        visited = [0] * len(nums)
+
+
+        #populate graph
+        for s, d in nums:
+            graph[s].add(d)
+
+        #bfs to search through the entire graph 
+        #since no start node is provided, assuming we will start at node 0
+        queue = [0]
+        while queue:
+
+            item = queue.pop(0)
+            visited[item] = 1
+
+            for neighbor in graph[item]:
+                if visited[neighbor] == 1:
+                    print("has cycle")
+                    return True
+                queue.append(neighbor)
+
+        print("no cycle")
+        return False
+
+
+dc = DetecyCycle()
+input_arr = [[0,1], [1,3], [1,2], [1,4], [3,7], [2,6], [7,8], [4,5],
+            [6,5], [8,9], [5,9], [9,8]]
+print(dc.detect_cycle(input_arr))
