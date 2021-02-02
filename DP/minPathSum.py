2d793097a986e57daef21fe916f55b75c502c91f
joypaul <jpaul3250@gmail.com>
2021-02-01 21:18:57 -0500

    revision

--- DP/minPathSum.py
+++ DP/minPathSum.py
@@ -34,3 +34,43 @@ class Solution(object):
 
         print(table)
         return table[-1][-1]
+
+
+'''
+Dijkstra approach!
+'''
+
+import heapq
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        height = len(grid)
+        width = len(grid[0])
+        target = (height-1, width-1)
+        
+        return self.dijkstra(grid, target, height, width)
+    
+    
+    def dijkstra(self, grid, target, height, width):
+        #prep with initial cost and the coordinates
+        q = [(grid[0][0], 0, 0)]
+        visited = set((0, 0))
+        
+        #start visiting
+        while q:
+            cost, row, col = heapq.heappop(q)
+            
+            if (row, col) == target:
+                return cost
+            
+            #we can get away with only visiting down and right
+            directions = [(row+1, col), (row, col+1)]
+            
+            #for each direction calculate updated cost and append to heap
+            for x, y in directions:
+                if 0<=x<height and 0<=y<width and (x,y) not in visited:
+                    #add the only directions we are visiting
+                    visited.add((x, y))
+                    
+                    newCost = grid[x][y] + cost
+                    heapq.heappush(q, (newCost, x, y))
